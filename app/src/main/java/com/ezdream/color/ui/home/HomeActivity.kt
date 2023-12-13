4cd735b74179bee3c2a304839629993876315a77
TunahanUnsal <tunahan.unsal@ceng.deu.edu.tr>
2023-12-14 00:38:54 +0300

    camera and gallery

---
+++ app/src/main/java/com/ezdream/color/ui/home/HomeActivity.kt
@@ -0,0 +1,128 @@
+package com.ezdream.color.ui.home
+
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.ezdream.color.R
+import com.ezdream.color.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private val REQUEST_CAMERA_PERMISSION = 101
+    private val REQUEST_GALLERY_PERMISSION = 202
+    private val REQUEST_IMAGE_CAPTURE = 303
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.cameraButton.setOnClickListener {
+            checkCameraPermission()
+        }
+
+        binding.galleryButton.setOnClickListener {
+            checkGalleryPermission()
+        }
+    }
+
+    private fun checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_CAMERA_PERMISSION
+            )
+        } else {
+            dispatchTakePictureIntent()
+        }
+    }
+
+    private fun checkGalleryPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_MEDIA_IMAGES
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_MEDIA_IMAGES),
+                    REQUEST_GALLERY_PERMISSION
+                )
+            }
+
+        } else {
+            openGallery()
+        }
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, REQUEST_GALLERY_PERMISSION)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CAMERA_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    dispatchTakePictureIntent()
+                } else {
+                    Toast.makeText(this, "Kamera izni reddedildi", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            REQUEST_GALLERY_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openGallery()
+                } else {
+                    Toast.makeText(this, "Galeri izni reddedildi", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK && data != null) {
+            val imageBitmap = data.extras?.get("data") as Bitmap?
+           // binding.test.setImageBitmap(imageBitmap)
+        } else if (requestCode == REQUEST_GALLERY_PERMISSION && resultCode == Activity.RESULT_OK && data != null) {
+            val selectedImage: Uri? = data.data
+            //binding.test.setImageURI(selectedImage)
+        }
+    }
+}
--- app/src/main/java/com/ezdream/color/ui/login/LoginActivity.kt
+++ app/src/main/java/com/ezdream/color/ui/login/LoginActivity.kt
@@ -1,11 +1,13 @@
 package com.ezdream.color.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.ezdream.color.R
 import com.ezdream.color.databinding.ActivityLoginBinding
+import com.ezdream.color.ui.home.HomeActivity
 import com.ezdream.color.util.UiUtil.getDuration
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,13 +28,13 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        val duration = getDuration(applicationContext,R.drawable.splash_3)
+        val duration = getDuration(applicationContext,R.drawable.splash_2)
         val background = object : Thread() {
             override fun run() {
                 try {
-                    sleep(duration.toLong() * 6)
-                    //val intent = Intent(baseContext, HomeActivity::class.java)
-                    //startActivity(intent)
+                    sleep(duration.toLong())
+                    val intent = Intent(baseContext, HomeActivity::class.java)
+                    startActivity(intent)
                     overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
                 }catch (e : Exception){
                     e.printStackTrace()
